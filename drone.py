55456cfef11714cb50f682a0ff41cf536736dc91
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2025-03-29 15:38:28 +0800

    feat(drone): parse question bank

--- drone.py
+++ drone.py
@@ -27,19 +27,26 @@ def get_args():
 def main():
     args = get_args()
     qs = extract_questions_from_pdf(args.file)
+    print(qs)
 
 
 def extract_questions_from_pdf(file):
-    questions = []
+    qs = []
     with pdfplumber.open(file) as pdf:
         t = ''
         q = Question()
         mode = 'question'
+        i = 0
         for page in pdf.pages:
             text = page.extract_text()
 
             for line in text.split('\n'):
                 if mode == 'question':
+                    if line == '第一章 民用航空法及相關法規答案':
+                        print('switch to answer mode')
+                        mode = 'answer'
+                        continue
+
                     m = re.match(r'第.章 (?P<tag>.*)$', line)
                     if m:
                         t = m.group('tag')
@@ -58,7 +65,7 @@ def extract_questions_from_pdf(file):
                         if choice.startswith('(D)'):
                             q.choice_d = choice
 
-                            questions.append(q)
+                            qs.append(q)
                             q = Question()
                         continue
 
@@ -68,18 +75,37 @@ def extract_questions_from_pdf(file):
                         q.tag = t
                         continue
 
-                    if text == '第一章 民用航空法及相關法規答案':
-                        mode = 'answer'
+                    if len(q.question) > 0 and q.question[-1] != '？':
+                        q.question += line
                         continue
 
-                    q.question += line
-
                 if mode == 'answer':
-                    pass
+                    no = ''
+                    for token in line.split(' '):
+                        m = re.match(r'(?P<no>\d+[.])', token)
+                        if m:
+                            no = m.group('no')
+                            continue
 
-    print(questions)
+                        if not qs[i].question.startswith(no):
+                            print(f'parsing error {i=}, {qs[i].question=} != {no=}')
+                            exit(-1)
+
+                        if token == 'A':
+                            qs[i].answer = qs[i].choice_a
+                            i += 1
+                        elif token == 'B':
+                            qs[i].answer = qs[i].choice_b
+                            i += 1
+                        elif token == 'C':
+                            qs[i].answer = qs[i].choice_c
+                            i += 1
+                        elif token == 'D':
+                            qs[i].answer = qs[i].choice_d
+                            i += 1
+
+    return qs
 
 
 if __name__ == '__main__':
-    sys.exit(main())
     sys.exit(main())
